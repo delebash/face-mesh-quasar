ed60358f6c21e8e74e847ec504b3e6a282e6a899
delebash <danelebash@hotmail.com>
2020-07-10 08:48:00 -0400

    tabs working ccs formated

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,49 +1,51 @@
 <template>
   <div>
-    <div>
-        <q-tab-panels v-model="tab" animated>
-          <q-tab-panel name="cameras">
-            <div class="text-h6">Cameras</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-
-          <q-tab-panel name="settings">
-            <div class="text-h6">Settings</div>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </q-tab-panel>
-        </q-tab-panels>
-
-        <q-separator/>
       <q-footer>
-        <q-tabs
-          v-model="tab"
-          dense
-          class="text-grey"
-          active-color="primary"
-          indicator-color="primary"
-          align="justify"
-          narrow-indicator
-        >
 
-            <q-tab name="cameras" label="Cameras"/>
-            <q-tab name="settings" label="Settings"/>
+
+        <q-tabs v-model="tab">
+          <q-tab name="images" label="Images" />
+          <q-tab name="videos" label="Videos" />
+          <q-tab name="articles" label="Articles" />
         </q-tabs>
       </q-footer>
 
-    </div>
+      <q-page-container class="no-padding">
+        <q-page class="row">
+          <q-tab-panels class="q-mt-md" v-model="tab" swipeable animated style="width: 100%">
+
+            <q-tab-panel name="images" style="background:red;">
+              <div class="text-h6 q-ma-md">Images</div>
+              <p class="q-ma-md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              <div
+                class="q-ma-md q-pa-md"
+                style="background:black;color:white;width:40%;height:30%;"
+              >
+                An image which wants to use the a percentage of the page
+              </div>
+            </q-tab-panel>
+
+            <q-tab-panel swipeable name="videos" style="background:green;">
+              <div class="text-h6 q-ma-md">Videos</div>
+              <p class="q-ma-md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </q-tab-panel>
+
+            <q-tab-panel swipeable name="articles" style="background:blue;">
+              <div class="text-h6 q-ma-md">Articles</div>
+              <p class="q-ma-md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            </q-tab-panel>
+
+          </q-tab-panels>
+        </q-page>
+      </q-page-container>
   </div>
 </template>
-
 <script>
 export default {
-  data() {
+  data () {
     return {
-      tab: ''
+      tab: 'images'
     }
   }
 }
 </script>
-
-<style scoped>
-
-</style>
